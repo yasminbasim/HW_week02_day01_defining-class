5740f7e2cbde9dcd258267dc057e992c2c332f06
yasminbasim <Yasmin.Basim.jed.k2@tuwaiq.edu.sa>
2021-10-18 02:26:50 +0300

    i did my best

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,26 +1,27 @@
 # Kotlin Programming
-
 fun main() {
-    val  ss=stu1("yasmin",23,12.9)
+    val ss = stu1("yasmin", 23, 12.9)
     stu1.speak()
     stu1.add(22, 1)
 }
-class stu1(var name1:String,var age:Int,var GPA:Double){
 
+class stu1(var name1:String,var age:Int,var GPA:Double) {
 
-}
-fun speak(){
     fun speak() {
-        println("my Name=$name1 Age $age GPA$ GPA")
+        fun speak() {
+            println("my Name $name1 and my age $age and my $GPA GPA")
+        }
+
+
+    }
+    fun add(n1: Int, n2: Int): Int {
+        n1 + n2
+
     }
 
 
-}
-fun add(n1:Int,n2:Int):Int{
-    n1+n2
 
-}
-## Learning Objectives ✨
+    ## Learning Objectives ✨
 - defining class
 ## Getting started 
 Open the [assignment.md](assignment.md) file to get started.
