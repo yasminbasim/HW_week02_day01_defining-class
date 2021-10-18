d2e5b3d26395f134e27a9eb7f9a7ed77c908a565
yasminbasim <Yasmin.Basim.jed.k2@tuwaiq.edu.sa>
2021-10-18 08:38:52 +0300

    i did my best

--- homeworkSolution.kt
+++ homeworkSolution.kt
@@ -1,25 +1,22 @@
-# Kotlin Programming
-ffun main() {
-    val ss = stu1("yasmin", 23, 12.9)
+fun main() {
+    var ss = stu1("yasmin", 23, 12.9)
 
-    stu1.speak("")
-    stu1.add(11,14)
+    ss.speak()
+    ss.add(11,14)
 
 }
 
-class stu1(private var name1: String, var age: Int, var GPA: Double) {
+class stu1( var name1: String, var age: Int, var GPA: Double) {
     fun speak() {
-
-        println("my  $name1\n and my  $age\n and my $GPA \n")
+        println("my name is $name1\n and my age is $age\n and my is GPA $GPA \n")
     }
 
     fun add(n1: Int, n2: Int): Int {
-        return n1 + n2
+        return (n1 + n2)
 
     }
 
 
 }
-
 
 
